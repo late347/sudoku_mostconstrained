ad7f582d5166fcd53f767b312cfd09ec2fe282b5
lauri solin <late347@gmail.com>
2022-04-28 05:40:28 +0300

    multithreading is still a bit bugged,, recursion is not progressing towards the smallest case

--- Board.cpp
+++ Board.cpp
@@ -249,6 +249,8 @@ Cell Board::getMostConstrainedEmpty()
 
     // we should always get a real empty cell into here
     Cell minCell = empties.front();
+    // MULTITHREADED BREAKPOINT
+    // THE RECURSION IS NOT PROGRESSING TO THE SMALLER CASES
     bool first = true;
     if (minCell.theValue != 0) 
     {
